253c1b88b795d98f40b1fa27ebd1bc8e91dce102
Andre <andre.cl@hotmail.com>
2021-01-28 15:29:39 +0100

    WIP cuestionaire

---
+++ EleccionesPeru2021/Shared/Models/UIModels/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharedLibrary.Models.UIModels
+{
+	public class Question
+	{
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public List<string> Options { get; set; }
+
+		public Action<string> OnNext { get; set; }
+
+		public void Next(string option)
+		{
+			OnNext?.Invoke(option);
+		}
+	}
+}
